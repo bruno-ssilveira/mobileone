a89a4d69c92c562318ba994440da98dec5addd71
bruno <bruno.santossilveira001@gmail.com>
2025-08-02 10:06:07 -0300

    DayListItem component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import DayListItem from './src/core/components/DayListItem';
+
+const date = new Date();
+const year = date.getFullYear();
+
+// inicia no mês atual. 0=janeiro, 1=fevereiro
+const month = date.getMonth();
+
+// Ano, mes, dia. mes+1 leva pro mes seguinte, dia 0 é o dia anterior ao primeiro dia do mes seguinte.
+const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+// O primeiro argumento (_) é o valor atual, que não usamos (por isso usamos _, uma convenção).
+// O segundo argumento (i) é o índice, que vai de 0 até daysInMonth - 1.
+// i + 1 faz com que os dias comecem em 1 ao invés de 0.
+const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Hello World!</Text>
+
+			<FlatList
+				data={daysArray}
+				contentContainerStyle={styles.content}
+				columnWrapperStyle={styles.column}
+				numColumns={3}
+				renderItem={ ({ item }) => <DayListItem day={item} /> }
+				//horizontal
+				style={styles.flatlist}
+			/>
+
+			{/*daysArray.map(day => (
+				<View style={styles.box} key={day}>
+					<Text style={styles.text}>{day}</Text>
+				</View>
+			))*/}
+
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -14,7 +45,19 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
 		justifyContent: 'center',
+
+		gap: 10,
 	},
+
+	content: {
+		gap: 10,
+	},
+	column: {
+		gap: 10,
+	},
+
+	flatlist: {
+		marginTop: 70,
+	}
 });
